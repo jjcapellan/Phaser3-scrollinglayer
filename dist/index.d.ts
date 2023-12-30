8626d2ea1c97b270a71abd4e712742277138e358
jjcapellan <soycape@hotmail.com>
2023-12-30 12:25:25 +0100

    build v3.0.0-alpha.2

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,43 +1,71 @@
-import Phaser from "phaser";
+/// <reference types="../node_modules/phaser/types/phaser.d.ts" />
 export default class ScrollingLayer {
     scene: Phaser.Scene;
     speed: number;
     texture: string;
     frame: string | undefined;
-    y: number;
+    position: number;
     overlap: number;
     width: number;
     height: number;
     origin: number;
     blitter: Phaser.GameObjects.Blitter;
-    img1: Phaser.GameObjects.Bob;
-    img2: Phaser.GameObjects.Bob;
+    _isH: number;
+    _isV: number;
+    _axis: string;
+    _img1: Phaser.GameObjects.Bob;
+    _img2: Phaser.GameObjects.Bob;
     /**
      *Creates an instance of ScrollingLayer.
      * @param scene
-     * @param speed - Horizontal speed in pixels/second.
+     * @param position - Position in the cross axis (y axis for horizontal layer
+     * and x axis for vertical layer)
+     * @param speed - Speed in pixels/second.
      * @param texture - Key of the texture stored in cache.
-     * @param [options]
-     * @param [options.frame] - Optional frame of the texture.
-     * @param [options.y = 0] - vertical position in pixels. By default the texture is positioned at bottom.
-     * @param [options.overlap = 0] - Horizontal overlap in pixels (default 1). Prevents empty spaces between images.
-     * @memberof ScrollingLayer
+     * @param [frame] - Frame of the texture. (Optional)
      */
-    constructor(scene: Phaser.Scene, speed: number, texture: string, options: Options);
-    getDistance(speed: number, deltaTime: number): number;
-    setOrigin(origin: number): ScrollingLayer;
-    setOverlap(overlap: number): ScrollingLayer;
-    setY(y: number): ScrollingLayer;
+    constructor(scene: Phaser.Scene, position: number, speed: number, texture: string, frame?: string);
+    _getDistance(speed: number, deltaTime: number): number;
+    _resetPosition(): void;
     /**
-     * Updates the x position.
+     * Sets the opacity
+     * @param alpha Number between 0 (transparent) and 1 (opaque)
+     * @returns ScrollingLayer
+     */
+    setAlpha(alpha: number): ScrollingLayer;
+    /**
+     * Sets layer orientation to vertical
+     * @returns ScrollingLayer
+     */
+    setVertical(): ScrollingLayer;
+    /**
+     * Sets layer orientation to horizontal (default)
+     * @returns ScrollingLayer
+     */
+    setHorizontal(): ScrollingLayer;
+    /**
+     * Sets the layer origin in the cross axis (x for horizontal and y for vertical layer)
+     * @param origin Number between 0 ( left/top ) and 1 ( right/bottom )
+     * @returns ScrollingLayer
+     */
+    setOrigin(origin: number): ScrollingLayer;
+    /**
+     * Sets overlap property.
+     * @param overlap Overlap in pixels (default 1). Prevents empty spaces between images.
+     * For semitransparent layers is recommended set value to 0.
+     * @returns ScrollingLayer
+     */
+    setOverlap(overlap: number): ScrollingLayer;
+    /**
+     * Sets the position
+     * @param position Position in the cross axis (y axis for horizontal layer
+     * and x axis for vertical layer)
+     * @returns ScrollingLayer
+     */
+    setPosition(position: number): ScrollingLayer;
+    /**
+     * Updates the layer position.
      * @param delta - Duration of last game step in miliseconds
-     * @memberof ScrollingLayer
      */
     update(delta: number): void;
 }
-type Options = {
-    frame?: string;
-    y?: number;
-    overlap?: number;
-};
-export {};
--- dist/scrollinglayer.esm.js
+++ dist/scrollinglayer.esm.js
@@ -1,65 +1,165 @@
-import Phaser from "phaser";
+if (IS_DEV) {
+  new EventSource("/esbuild").addEventListener("change", () => location.reload());
+}
 class ScrollingLayer {
   /**
    *Creates an instance of ScrollingLayer.
-   * @param scene     
-   * @param speed - Horizontal speed in pixels/second.    
+   * @param scene
+   * @param position - Position in the cross axis (y axis for horizontal layer
+   * and x axis for vertical layer)
+   * @param speed - Speed in pixels/second.    
    * @param texture - Key of the texture stored in cache.
-   * @param [options]
-   * @param [options.frame] - Optional frame of the texture.
-   * @param [options.y = 0] - vertical position in pixels. By default the texture is positioned at bottom.
-   * @param [options.overlap = 0] - Horizontal overlap in pixels (default 1). Prevents empty spaces between images.
-   * @memberof ScrollingLayer
+   * @param [frame] - Frame of the texture. (Optional)
    */
-  constructor(scene, speed, texture, options) {
+  constructor(scene, position, speed, texture, frame) {
     this.scene = scene;
+    this.position = position;
     this.speed = speed;
     this.texture = texture;
-    this.frame = options.frame;
-    this.overlap = options.overlap || 1;
+    this.frame = frame;
+    this.overlap = 1;
+    this._isH = 1;
+    this._isV = 0;
+    this._axis = "x";
+    this.origin = 0;
     this.width = this.scene.textures.getFrame(this.texture, this.frame).width;
     this.height = this.scene.textures.getFrame(this.texture, this.frame).height;
-    this.origin = 0;
-    this.y = options.y || scene.game.config.height - this.height;
-    this.blitter = this.scene.add.blitter(0, this.y, this.texture, this.frame);
-    this.img1 = this.blitter.create(0, 0);
-    this.img2 = this.blitter.create(this.width - this.overlap, 0);
+    this.blitter = this.scene.add.blitter(
+      this._isV * this.position + 0,
+      this._isH * this.position + 0,
+      this.texture,
+      this.frame
+    );
+    this._img1 = this.blitter.create(0, 0);
+    this._img2 = this.blitter.create(
+      (this.width - this.overlap) * this._isH + 0,
+      (this.height - this.overlap) * this._isV + 0
+    );
   }
-  getDistance(speed, deltaTime) {
+  _getDistance(speed, deltaTime) {
     return deltaTime * speed / 1e3;
   }
-  setOrigin(origin) {
-    this.origin = Phaser.Math.Clamp(origin, 0, 1);
+  _resetPosition() {
+    this.blitter.setPosition(
+      this._isV * this.position + 0,
+      this._isH * this.position + 0
+    );
+    this._img1.setPosition(0, 0);
+    this._img2.setPosition(
+      (this.width - this.overlap) * this._isH + 0,
+      (this.height - this.overlap) * this._isV + 0
+    );
+  }
+  /**
+   * Sets the opacity
+   * @param alpha Number between 0 (transparent) and 1 (opaque)
+   * @returns ScrollingLayer
+   */
+  setAlpha(alpha) {
+    alpha = Math.max(0, alpha);
+    this.blitter.setAlpha(alpha);
     return this;
   }
+  /**
+   * Sets layer orientation to vertical
+   * @returns ScrollingLayer
+   */
+  setVertical() {
+    this._isV = 1;
+    this._isH = 0;
+    this._axis = "v";
+    this._resetPosition();
+    return this;
+  }
+  /**
+   * Sets layer orientation to horizontal (default)
+   * @returns ScrollingLayer
+   */
+  setHorizontal() {
+    this._isV = 0;
+    this._isH = 1;
+    this._axis = "x";
+    this._resetPosition();
+    return this;
+  }
+  /**
+   * Sets the layer origin in the cross axis (x for horizontal and y for vertical layer)
+   * @param origin Number between 0 ( left/top ) and 1 ( right/bottom )
+   * @returns ScrollingLayer
+   */
+  setOrigin(origin) {
+    this.origin = Phaser.Math.Clamp(origin, 0, 1);
+    this.setPosition(this.position);
+    return this;
+  }
+  /**
+   * Sets overlap property.
+   * @param overlap Overlap in pixels (default 1). Prevents empty spaces between images. 
+   * For semitransparent layers is recommended set value to 0.
+   * @returns ScrollingLayer
+   */
   setOverlap(overlap) {
     this.overlap = Math.max(0, overlap);
     return this;
   }
-  setY(y) {
-    this.y = y;
-    this.blitter.y = y - this.origin * this.height;
+  /**
+   * Sets the position
+   * @param position Position in the cross axis (y axis for horizontal layer
+   * and x axis for vertical layer)
+   * @returns ScrollingLayer
+   */
+  setPosition(position) {
+    this.position = position;
+    if (this._isH) {
+      this.blitter.y = position - this.origin * this.height;
+      return this;
+    }
+    this.blitter.x = position - this.origin * this.width;
     return this;
   }
   /**
-   * Updates the x position.
+   * Updates the layer position.
    * @param delta - Duration of last game step in miliseconds
-   * @memberof ScrollingLayer
    */
   update(delta) {
-    this.img1.x += this.getDistance(this.speed, delta);
-    this.img2.x += this.getDistance(this.speed, delta);
-    if (this.speed < 0 && this.img1.x < -this.width) {
-      this.img1.x = this.width + this.img2.x - this.overlap;
+    const distance = this._getDistance(this.speed, delta);
+    if (this._isH) {
+      this._img1.x += distance;
+      this._img2.x += distance;
+      if (this.speed < 0 && this._img1.x < -this.width) {
+        this._img1.x = this.width + this._img2.x - this.overlap;
+        return;
+      }
+      if (this.speed < 0 && this._img2.x < -this.width) {
+        this._img2.x = this.width + this._img1.x - this.overlap;
+        return;
+      }
+      if (this.speed > 0 && this._img1.x > this.width) {
+        this._img1.x = -this.width + this._img2.x + this.overlap;
+        return;
+      }
+      if (this.speed > 0 && this._img2.x > this.width) {
+        this._img2.x = -this.width + this._img1.x + this.overlap;
+        return;
+      }
+      return;
     }
-    if (this.speed < 0 && this.img2.x < -this.width) {
-      this.img2.x = this.width + this.img1.x - this.overlap;
+    this._img1.y += distance;
+    this._img2.y += distance;
+    if (this.speed < 0 && this._img1.y < -this.height) {
+      this._img1.y = this.height + this._img2.y - this.overlap;
+      return;
     }
-    if (this.speed > 0 && this.img1.x > this.width) {
-      this.img1.x = -this.width + this.img2.x + this.overlap;
+    if (this.speed < 0 && this._img2.y < -this.height) {
+      this._img2.y = this.height + this._img1.y - this.overlap;
+      return;
     }
-    if (this.speed > 0 && this.img2.x > this.width) {
-      this.img2.x = -this.width + this.img1.x + this.overlap;
+    if (this.speed > 0 && this._img1.y > this.height) {
+      this._img1.y = -this.height + this._img2.y + this.overlap;
+      return;
+    }
+    if (this.speed > 0 && this._img2.y > this.height) {
+      this._img2.y = -this.height + this._img1.y + this.overlap;
     }
   }
 }
