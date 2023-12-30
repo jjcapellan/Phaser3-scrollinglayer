8fed30048853a0a3b8ef44d07baed63dc60de4f9
jjcapellan <soycape@hotmail.com>
2023-12-30 20:00:03 +0100

    Demo: v3.0.0-alpha.3

--- demo/scenes/parallax.js
+++ demo/scenes/parallax.js
@@ -1,7 +1,4 @@
-/*let count = 0;
-let acc = 0;
-let avg = 0;*/
-
+/// <reference types="../../node_modules/phaser/types/phaser.d.ts" />
 class Parallax extends Phaser.Scene {
   constructor() {
     super('parallax');
@@ -20,46 +17,37 @@ class Parallax extends Phaser.Scene {
     this.generateScrollingLayers();
   }
 
-  update(time, delta) {
-    //let t1 = performance.now();
-    this.updateScrollingLayers(delta);
-    /*let elapsed = performance.now() - t1;
-    count++;
-    avg = (elapsed + acc) / count;
-    acc += elapsed;
-    if(count == 2000) console.log(avg);*/
-  }
-
-  updateScrollingLayers(delta) {
-    this.layer4.update(delta);
-    this.layer3.update(delta);
-    this.layer2.update(delta);
-    this.layer1.update(delta);
-    this.layerRain.update(delta);
-  }
-
   generateScrollingLayers() {
     const factor = 2;
     let bottom = this.scale.height;
 
-    this.layer4 = new ScrollingLayer(this, 0, -20 * factor, 'atlas', 'Builds3-0')
+    const layers = this.add.existing(new Layers(this, "atlas"));
+
+    layers.addHlayer(0, -20 * factor, 'Builds3-0')
       .setOrigin(1)
       .setPosition(bottom - 60);
 
-    this.layer3 = new ScrollingLayer(this, 0, -40 * factor, 'atlas', 'Builds2-0')
+    layers.addHlayer(0, -40 * factor, 'Builds2-0')
       .setOrigin(0)
       .setPosition(bottom - 106 - 10);
 
-    this.layer2 = new ScrollingLayer(this, 0, -80 * factor, 'atlas', 'Builds1-0')
+    // let l = layers.addHlayer(0, 80 * factor, 'Builds1-0')
+    let l = layers.addHlayer(0, -80 * factor, 'Builds1-0')
       .setOrigin(1)
       .setPosition(bottom - 10);
 
-    this.layerRain = new ScrollingLayer(this, 0, 200, 'atlas', 'Rain-0')
-      .setOrigin(1)
-      .setVertical()
+    
+    // this.time.delayedCall(5000, () => { l.setVisible(false)});
+    // this.time.delayedCall(5000, () => { layers.destroy()});
+    // this.time.delayedCall(5000, () => { layers.destroy(true)});
+    // this.time.delayedCall(5000, () => { layers.removeLayer(l); });
+    // this.time.delayedCall(5000, () => { l.setSpeed(40)});
+
+    // layers.addVlayer(0, -200, 'Rain-0') 
+    layers.addVlayer(0, 200, 'Rain-0')
       .setAlpha(0.08);
 
-    this.layer1 = new ScrollingLayer(this, bottom, -160 * factor, 'atlas', 'Front-0')
+    layers.addHlayer(bottom, -160 * factor, "Front-0")
       .setOrigin(1);
   }
 }
