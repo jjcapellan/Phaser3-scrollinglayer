659fefc75383cd5023b51b4782d17b98546f2f25
jjcapellan <soycape@hotmail.com>
2023-12-31 12:48:34 +0100

    Added Layer.setFrame()

--- src/layer.ts
+++ src/layer.ts
@@ -18,12 +18,23 @@ export class Layer {
     /**
      * Sets the opacity.
      * @param alpha Number between 0 (transparent) and 1 (opaque) 
-     * @returns 
+     * @returns Instance of the layer
      */
     setAlpha(alpha: number): Layer {
         alpha = Math.max(0, alpha);
         this.data.img1.setAlpha(alpha);
         this.data.img2.setAlpha(alpha);
+        return this;
+    }
+
+    /**
+     * Sets the current frame used by the layer.
+     * @param frame The frame must be part of the Texture the parent LayerFactory is using.
+     * @returns Instance of the layer
+     */
+    setFrame(frame: string): Layer {
+        this.data.img1.setFrame(frame);
+        this.data.img2.setFrame(frame);
         return this;
     }
 
